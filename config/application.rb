fe2c6a24787a7d49a60249e615a4a1d084a84d4d
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-19 22:02:29 +0100

    wikinimous created

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module RailsWikinimous
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators.system_tests = nil
   end
 end
